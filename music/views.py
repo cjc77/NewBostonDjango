a32c0ced77a636c6aac686a5cd8e00075e5a41e6
carson <carsonjamescook@gmail.com>
2016-12-10 21:37:29 -0800

    Whole bunch of new stuff. Index.html and updated views.index.

--- music/views.py
+++ music/views.py
@@ -1,6 +1,16 @@
 from django.http import HttpResponse
+from django.template import loader
+from . models import Album
+
 
 def index(request):
-    return HttpResponse("YO."
-                        "<h1>This is the music homepage</h1>"
-                        "<p>You like?</p>")
+    all_albums = Album.objects.all()
+    template = loader.get_template('music/index.html')
+    context = {
+        "all_albums": all_albums,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, album_id):
+    return HttpResponse("<h2>Details for Album ID: " + str(album_id) + "</h2>")
