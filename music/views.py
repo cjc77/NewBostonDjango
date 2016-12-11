2780e41c4105f4a12fa391f108d2e0274df1a82c
carson <carsonjamescook@gmail.com>
2016-12-10 21:53:13 -0800

    Added a template to the detail view for individual albums.

--- music/views.py
+++ music/views.py
@@ -1,16 +1,23 @@
+from django.http import Http404
 from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 from . models import Album
 
 
 def index(request):
     all_albums = Album.objects.all()
-    template = loader.get_template('music/index.html')
     context = {
         "all_albums": all_albums,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, "music/index.html", context)
 
 
 def detail(request, album_id):
-    return HttpResponse("<h2>Details for Album ID: " + str(album_id) + "</h2>")
+    try:
+        album = Album.objects.get(pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404("This Album does not currently exist.")
+    context = {
+        "album": album,
+    }
+    return render(request, "music/detail.html", context)
