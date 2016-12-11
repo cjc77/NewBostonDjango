e5729984a88e87e47a0b8533529da2a0b3fdcecb
carson <carsonjamescook@gmail.com>
2016-12-10 22:48:42 -0800

    Hard-coded urls removed and "detail" specified for music.

--- music/urls.py
+++ music/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 from . import views
 
+app_name = "music"
 
 urlpatterns = [
+    # 'name' represents the associated url pattern
     url(r'^$', views.index, name='index'),
     url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
 ]
