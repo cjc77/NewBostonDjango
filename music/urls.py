0f9df557ea808a19a5b07062e24b2630b989e77d
carson <carsonjamescook@gmail.com>
2016-12-11 22:14:13 -0800

    Added form to add new albums.

--- music/urls.py
+++ music/urls.py
@@ -6,7 +6,9 @@ app_name = "music"
 urlpatterns = [
     # 'name' represents the associated url pattern
     url(r'^$', views.IndexView.as_view(), name='index'),
+    # /music/pk/
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
-
+    # /music/album/add/
+    url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
 
 ]
--- music/views.py
+++ music/views.py
@@ -1,4 +1,5 @@
 from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from . models import Album, Song
 
 
@@ -13,3 +14,7 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Album
     template_name = 'music/detail.html'
+
+class AlbumCreate(CreateView):
+    model = Album
+    fields = ['artist', 'album_title', 'genre', 'album_logo']
