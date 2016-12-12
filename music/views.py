067ff9ac7261a2c8ff93f12c462f1e0029892e7f
carson <carsonjamescook@gmail.com>
2016-12-12 00:31:20 -0800

    User registration.

--- music/views.py
+++ music/views.py
@@ -1,7 +1,11 @@
 from django.views import generic
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.core.urlresolvers import reverse_lazy
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.views.generic import View
 from . models import Album, Song
+from . forms import UserForm
 
 
 class IndexView(generic.ListView):
@@ -29,3 +33,44 @@ class AlbumUpdate(UpdateView):
 class AlbumDelete(DeleteView):
     model = Album
     success_url = reverse_lazy('music:index')
+
+
+class UserFormView(View):
+    form_class = UserForm
+    template_name = 'music/registration_form.html'
+
+    # display a blank form
+    def get(self, request):
+        form = self.form_class(None)
+        context = {
+            'form': form,
+        }
+        return render(request, self.template_name, context)
+
+    # process form
+    def post(self, request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+
+            # hasn't been saved to db yet
+            user = form.save(commit=False)
+
+            # cleaned (normalized) data
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user.set_password(password)
+            user.save()
+
+
+
+
+
+
+
+
+
+
+
+
+    #
