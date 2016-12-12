fe12352bcf6b4554f3fdba7ee8ae9218a3f7e0cf
carson <carsonjamescook@gmail.com>
2016-12-11 22:53:53 -0800

    Added deletion and updating capabilities.

--- music/urls.py
+++ music/urls.py
@@ -6,9 +6,16 @@ app_name = "music"
 urlpatterns = [
     # 'name' represents the associated url pattern
     url(r'^$', views.IndexView.as_view(), name='index'),
+
     # /music/pk/
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+
     # /music/album/add/
     url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
 
+    # /music/album/pk/
+    url(r'album/(?P<pk>[0-9]+)/$', views.AlbumUpdate.as_view(), name='album-update'),
+
+    # /music/album/pk/delete/
+    url(r'album/(?P<pk>[0-9]+)/delete$', views.AlbumDelete.as_view(), name='album-delete'),
 ]
